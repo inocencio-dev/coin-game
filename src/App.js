1aba04b077c75b38bcd204c01b6f4293a9e555bf
Mark Mitkevich <mark.mitkevich@celadonsoft.com>
2022-02-03 20:21:11 +0300

    preparations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlqam9g6Ru50Ei9tmTj8rd_R5Q3OpuMxw",
+  authDomain: "celadon-edu2022.firebaseapp.com",
+  projectId: "celadon-edu2022",
+  storageBucket: "celadon-edu2022.appspot.com",
+  messagingSenderId: "564944460968",
+  appId: "1:564944460968:web:b75dd5dd75b5b663eeb69d"
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
